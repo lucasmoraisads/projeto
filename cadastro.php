f33a02ffd9ac28c07826307f000ade41617aeef7
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2023-02-02 22:33:32 -0300

    consegui

---
+++ cadastro.php
@@ -0,0 +1,10 @@
+<?php
+ 
+echo $nome = $_POST['nome']; 
+echo "<br>";
+echo $sobrenome = $_POST['sobrenome'];
+echo "<br>";
+echo $email = $_POST['email'];
+echo "<br>";
+echo $sexo = $_POST['sexo'];
+echo "<br>";
