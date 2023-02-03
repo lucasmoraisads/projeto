cadb3aa1578a601f6b8d81a54ab23b75f219e21f
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2023-02-03 13:30:12 -0300

    mudacas

--- cadastro.php
+++ cadastro.php
@@ -1,10 +1,27 @@
 <?php
- 
+
+
 echo $nome = $_POST['nome']; 
-echo "<br>";
 echo $sobrenome = $_POST['sobrenome'];
-echo "<br>";
 echo $email = $_POST['email'];
-echo "<br>";
 echo $sexo = $_POST['sexo'];
-echo "<br>";
+
+
+if ($nome == null){
+     echo "Nenhuma informação adicionada!" ;
+}
+elseif($sobrenome == null){
+    echo "Nenhuma informação adicionada!" ; 
+}
+elseif($email == null){
+    echo "Nenhuma informação adicionada!" ;
+}
+elseif($sexo == null){
+    echo "Nenhuma informação adicionada!" ;
+}
+else{
+    echo $nome . '<br>';
+    echo $sobrenome . '<br>';
+    echo $email . '<br>';
+    echo $sexo . '<br>'; 
+}
